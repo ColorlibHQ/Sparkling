e9239ab8a6edd7ec137de5b62588a3694c0c29e2
puikinsh <aigars.silkalns@gmail.com>
2014-05-13 11:07:38 +0300

    Removed dublicate class

--- page-fullwidth.php
+++ page-fullwidth.php
@@ -8,7 +8,7 @@
  */
 
 get_header(); ?>
-<div id="content" class="site-content container">
+<div id="content" class="site-content">
 	<div id="primary" class="content-area col-sm-12 col-md-12">
 		<main id="main" class="site-main" role="main">
 
