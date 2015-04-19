197846d7ee541085afc3840fb6befc5a07dde09f
puikinsh <aigars.silkalns@gmail.com>
2015-04-20 00:03:55 +0300

    Code styling improvements

--- functions.php
+++ functions.php
@@ -105,13 +105,13 @@ add_action( 'after_setup_theme', 'sparkling_setup' );
  */
 function sparkling_widgets_init() {
   register_sidebar( array(
-  	'name'          => __( 'Sidebar', 'sparkling' ),
-  	'id'            => 'sidebar-1',
-  	'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-  	'after_widget'  => '</aside>',
-  	'before_title'  => '<h3 class="widget-title">',
-  	'after_title'   => '</h3>',
-  ) );
+    'name'          => __( 'Sidebar', 'sparkling' ),
+    'id'            => 'sidebar-1',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget'  => '</aside>',
+    'before_title'  => '<h3 class="widget-title">',
+    'after_title'   => '</h3>',
+  ));
 
   register_sidebar(array(
     'id'            => 'home-widget-1',
