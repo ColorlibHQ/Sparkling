c3ff3fa3bc7c6c16c11433133f026106a2efed36
Rhiski Aprilianto <mail@rhiskiapril.com>
2015-05-21 17:09:04 +0700

    solve the problem of "Google Sturctured Data 'missing: updated'" on the post that has not been revised / updated see http://i.imgur.com/5O2K1Mm.png?1

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -71,10 +71,7 @@ if ( ! function_exists( 'sparkling_posted_on' ) ) :
  * Prints HTML with meta information for the current post-date/time and author.
  */
 function sparkling_posted_on() {
-	$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string .= '<time class="updated" datetime="%3$s">%4$s</time>';
-	}
+	$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
 
 	$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
