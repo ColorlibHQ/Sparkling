00b26b94d3d6caf628f38de11faabdb952efcfb2
puikinsh <aigars.silkalns@gmail.com>
2015-07-08 17:09:18 +0300

    Updated search form and translations

--- searchform.php
+++ searchform.php
@@ -6,9 +6,10 @@
  */
 ?>
 
-<form method="get" class="form-search" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form role="search" method="get" class="form-search" action="<?php echo esc_url( home_url( '/' ) ); ?>">
   <div class="input-group">
-    <input type="text" class="form-control search-query" value="<?php echo esc_attr( get_search_query() ); ?>" name="s" id="s" placeholder="<?php echo esc_attr_x( 'Search...', 'placeholder', 'sparkling' ); ?>">
+  	<label class="screen-reader-text" for="s"><?php _e( 'Search for:', 'sparkling' ); ?></label>
+    <input type="text" class="form-control search-query" placeholder="<?php echo esc_attr_x( 'Search&hellip;', 'placeholder', 'sparkling' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'sparkling' ); ?>" />
     <span class="input-group-btn">
       <button type="submit" class="btn btn-default" name="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'sparkling' ); ?>"><span class="glyphicon glyphicon-search"></span></button>
     </span>
