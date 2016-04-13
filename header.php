61b6e16e4501caae4ed03726396bae2fa0a12ccb
Marko Megla <markomegla@users.noreply.github.com>
2016-04-13 20:40:19 +0200

    Added the color of address bar in mobile browser
     The color of address bar in mobile browser (Chrome) matches top nav background color

--- header.php
+++ header.php
@@ -22,6 +22,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <?php if (isset($_SERVER['HTTP_USER_AGENT']) && (strpos($_SERVER['HTTP_USER_AGENT'], 'MSIE') !== false)) header('X-UA-Compatible: IE=edge,chrome=1'); ?>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="theme-color" content="<?php echo of_get_option( 'nav_bg_color' ); ?>">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
