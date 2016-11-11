5cefd027f131d92814ee45e68c1c331f2994b0ad
Free WP TP <freewptp@gmail.com>
2016-11-11 20:50:25 +0530

    Resolved the issue - If the menu link is created to open in new tab then it opens two tabs in firefox browser - #103

--- inc/extras.php
+++ inc/extras.php
@@ -459,9 +459,6 @@ if ( !wp_is_mobile() ) { ?>
         $( '.navbar-nav > li.menu-item > a' ).click( function(){
             if( $( this ).attr('target') !== '_blank' ){
                 window.location = $( this ).attr( 'href' );
-            }else{
-                var win = window.open($( this ).attr( 'href' ), '_blank');
-                win.focus();
             }
         });
       }
