24f86fedceb39b0b0c900d5a47e2b50fd48a9db7
Raiber Cristian <cristianraiber@users.noreply.github.com>
2017-06-08 20:00:07 +0300

    Revert "First development version update"

--- archive.php
+++ archive.php
@@ -31,7 +31,7 @@
  				 * If you want to override this in a child theme, then include a file
  				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
  				 */
- 				get_template_part( 'template-parts/content', get_post_format() );
+ 				get_template_part( 'content', get_post_format() );
 
  			endwhile;
 
@@ -42,7 +42,7 @@
 
  		else :
 
- 			get_template_part( 'template-parts/content', 'none' );
+ 			get_template_part( 'content', 'none' );
 
  		endif; ?>
 
