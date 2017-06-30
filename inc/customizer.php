1f5bb80c41c12f5bbbfdf30e761c77ddb2fa6f8b
Giucu <giucu91@yahoo.com>
2017-06-30 11:03:59 +0300

    Moved assets.

--- inc/customizer.php
+++ inc/customizer.php
@@ -700,7 +700,7 @@ function sparkling_sanitize_typo_style( $input ) {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function sparkling_customize_preview_js() {
-	wp_enqueue_script( 'sparkling_customizer', get_template_directory_uri() . '/inc/js/customizer.js', array( 'customize-preview' ), '20140317', true );
+	wp_enqueue_script( 'sparkling_customizer', get_template_directory_uri() . '/assets/js/customizer.js', array( 'customize-preview' ), '20140317', true );
 }
 add_action( 'customize_preview_init', 'sparkling_customize_preview_js' );
 
