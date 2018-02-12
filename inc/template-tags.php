bf1c165bd0ec4b26574f68f02d9baefddeba4ed7
Arthur Kelsch <akelsch@users.noreply.github.com>
2018-02-12 19:24:02 +0100

    Always include updated timestamp

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -34,9 +34,7 @@ if ( ! function_exists( 'sparkling_posted_on' ) ) :
  */
 	function sparkling_posted_on() {
 		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
-		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-			$time_string .= '<time class="updated" datetime="%3$s">%4$s</time>';
-		}
+					. '<time class="updated" datetime="%3$s">%4$s</time>';
 
 		$time_string = sprintf( $time_string,
 			esc_attr( get_the_date( 'c' ) ),
