d3f40254997faddc397ac907ef74c9949615f91f
Cristian Raiber <cristian@machothemes.com>
2018-05-03 09:58:19 +0300

    fixes #202

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -34,7 +34,7 @@ if ( ! function_exists( 'sparkling_posted_on' ) ) :
  */
 	function sparkling_posted_on() {
 		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-		
+
 		/*
 		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
 			$time_string .= '<time class="updated" datetime="%3$s">%4$s</time>';
