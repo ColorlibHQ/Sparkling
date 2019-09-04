bd380f586e2b3201a286e67f5b4a747e64daf452
George Ciobanu <george.ciobanu91@gmail.com>
2019-09-04 13:31:29 +0300

    fixed welcome screen

--- inc/welcome-screen/sections/support.php
+++ inc/welcome-screen/sections/support.php
@@ -1,7 +1,7 @@
 
-<div class="feature-section three-col">
+<div class="feature-section three-col has-3-columns is-fullwidth">
 
-	<div class="col">
+	<div class="col column">
 		<h3><i class="dashicons dashicons-sos"></i><?php esc_html_e( 'Contact Support', 'sparkling' ); ?></h3>
 		<p>
 			<i><?php esc_html_e( 'We offer excellent support through our advanced ticketing system. Make sure to register your purchase before contacting support!', 'sparkling' ); ?></i>
@@ -11,7 +11,7 @@
 		</p>
 	</div><!--/.col-->
 
-	<div class="col">
+	<div class="col column">
 		<h3><i class="dashicons dashicons-book-alt"></i><?php esc_html_e( 'Documentation', 'sparkling' ); ?></h3>
 		<p>
 			<i><?php esc_html_e( 'This is the place to go to reference different aspects of the theme. Our online documentation is an incredible resource for learning the ins and outs of using Sparkling.', 'sparkling' ); ?></i>
@@ -21,7 +21,7 @@
 		</p>
 	</div><!--/.col-->
 
-	<div class="col">
+	<div class="col column">
 		<h3><i class="dashicons dashicons-portfolio"></i><?php esc_html_e( 'Changelog', 'sparkling' ); ?></h3>
 		<p>
 			<i><?php esc_html_e( 'Want to get the gist on the latest theme changes? Just consult our changelog below to get a taste of the recent fixes and features implemented.', 'sparkling' ); ?></i>
